9fd3a4eddd06c408362e2d8a773397d57cc4247b
Suraj-dyp <surajdypcoe@gmail.com>
2019-11-17 15:22:34 +0530

    error corrected

--- remove.py
+++ remove.py
@@ -7,10 +7,13 @@ for l in f1:
 	os.system('sed -n /'+l1+'/= data/src-train.txt >> t.txt')
 f1.close()
 
+l2=''
 f2 = open('t.txt')
 for l in f2:
 	l1 = l.rstrip()
-	print(l1)
-	os.system('sed -i '+l1+'d data/tgt-train.txt')
-	os.system('sed -i '+l1+'d data/src-train.txt')
+	l2 += l1 + 'd;'
+l2 = l2[:-1]
+print(l2)
+os.system('sed -i "'+l2+'" data/tgt-train.txt')
+os.system('sed -i "'+l2+'" data/src-train.txt')
 f2.close()
